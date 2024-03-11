01b3ab3b09ae3a1120be0b8c13b4f75b8cee3f9a
thomas.prior@student.manchester.ac.uk <thomas.prior@student.manchester.ac.uk>
2024-03-11 19:26:45 +0000

    Wrote out ConsoleUtils and documented it.

--- src/main/util/ConsoleUtils.java
+++ src/main/util/ConsoleUtils.java
@@ -1,6 +1,6 @@
 package util;
-import java.util.List;
-import java.io.Console;
+import java.util.*;
+import java.io.*;
 
 /**
  * Helper methods for console i/o.
@@ -9,7 +9,7 @@ import java.io.Console;
 
 public class ConsoleUtils {
 
-    public Console console = system.console();
+    public Console console;
 
     /**
      * Helper methods for console i/o.
@@ -18,42 +18,127 @@ public class ConsoleUtils {
     
     public ConsoleUtils() {
 
+	console = System.console();
+
 	/**
 	 * Helper methods for console i/o.
 	 * @author thomas
 	 */
     }
 
+    /**
+     * Wrapped around base method.
+     * @return Input from user
+     */
+
     public String readLine() {
 
-	input = console.readLine("");
+	String input = console.readLine("");
 
 	return input;
 
     }
 
+    /**
+     * Read line and format it.
+     * @param fmt readLine customisations
+     * @param args  readLine customisations
+     * @return formatted string
+     */
+
     public String readLine(String fmt, Object... args) {
 
-	input = console.readLine("");
+	String input = console.readLine(fmt, args);
+
+	return input;
 
     }
+
+    /**
+     * Get file path
+     * @param Request
+     * @return File object
+     */
 
     public File promptForFilePath(String prompt) {
 
-	
+	String input = console.readLine(prompt);
+
+	File path = new File(input);
+
+	return path;
 
     }
 
-    // public List<String> promptForNewPlayers(String prompt) {
+    /**
+     * Ask for player names.
+     * @param prompt Request
+     * @return Parsed input as strings.
+     */
 
-    // }
+    public List<String> promptForNewPlayers(String prompt) {
 
-    // public boolean promptForStartLoad(String prompt) {
+	List<String> players = new ArrayList<>();
 
-    // }
+	String input = console.readLine(prompt);
 
-    // public boolean promptForYesNo(String prompt) {
+	while (input != null && players.size() < 2) {
 
-    // }
+	    if ( players.contains(input) ) {
+
+	    } else { players.add(input); }
+
+	    input = console.readLine(prompt);
+
+	    if (players.size() == 5) {
+
+		input = null;
+
+	    }
+
+	}
+
+	return players;
+
+    }
+
+    /**
+     * @param prompt Request
+     * @return Result as boolean.
+     */
+
+    public boolean promptForStartLoad(String prompt) {
+
+	String input = console.readLine(prompt + "[S]tart/[L]oad");
+
+	if (input.toLowerCase().contains("s")) {
+
+	    return true;
+
+	}
+
+	return false;
+
+    }
+
+    /**
+     * Yes/no input
+     * @param prompt Request
+     * @return Result as boolean.
+     */
+
+    public boolean promptForYesNo(String prompt) {
+
+	String input = console.readLine(prompt + "[Y]es/[N]o");
+
+	if (input.toLowerCase().contains("y")) {
+
+	    return true;
+
+	}
+
+	return true;
+
+    }
 
 }
