8d0420efd60798e181e97b81788f7c00274aa537
thomas.prior@student.manchester.ac.uk <thomas.prior@student.manchester.ac.uk>
2024-03-11 20:38:04 +0000

    Attempting the barest structural tests only.

--- src/main/bakery/Player.java
+++ src/main/bakery/Player.java
@@ -1,1 +1,58 @@
 package bakery;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Player  {
+
+    private String name;
+
+    private List<Ingredient> hand;
+
+    private static long serialVersionUID;
+
+    public Player(String name) {
+
+	this.name = name;
+
+    }
+
+    public void addToHand(List<Ingredient> ingredients) {
+
+    }
+
+    public void addToHand(Ingredient ingredient) {
+
+    }
+
+    public boolean hasIngredient(Ingredient ingredient) {
+
+	return true;
+
+    }
+
+    public void removeFromHand(Ingredient ingredient) {
+
+    }
+
+    public List<Ingredient> getHand() {
+
+	List<Ingredient> hand = new ArrayList<>();
+
+	return hand;
+
+    }
+
+    public String getHandStr() {
+
+	return "A";
+
+    }
+
+    public String toString() {
+
+	return name;
+
+    }
+
+}
