5bab7690d8b205972a991f23a239f6aa21350e2e
thomas.prior@student.manchester.ac.uk <thomas.prior@student.manchester.ac.uk>
2024-03-11 12:47:59 +0000

    Skeleton for UML stage 2, need some clarification on UML, or to check final.

--- src/main/util/CardUtils.java
+++ src/main/util/CardUtils.java
@@ -2,6 +2,30 @@ package util;
 
 public class CardUtils {
 
+    public static List<CustomerOrder> readCustomerFile(String path, Collection<Layer> layers) {
+
+    }
+
+    public static List<Ingredient> readIngredientFile(String path) {
+
+    }
+
+    public static List<Layer> readLayerFile(String path) {
+
+    }
+
+    private static CustomerOrder stringToCustomerOrder(String str) {
+
+    }
+
+    private static List<Ingredient> stringToIngredients(String str) {
+
+    }
+
+    private static List<Layer> stringToLayers(String str) {
+
+    }
+
     public CardUtils {
 
     }
